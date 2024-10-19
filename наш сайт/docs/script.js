a6f5e95225510833ff460b1bf2c1dda68f51504b
egorvostochny <hovachik337@gmail.com>
2024-10-19 04:57:21 +0300

    Add files via upload

---
+++ наш сайт/docs/script.js
@@ -0,0 +1,22 @@
+console.log("Добро пожаловать на мой сайт!");
+// Получаем элементы
+const contactBtn = document.getElementById('contact-btn');
+const modal = document.getElementById('contact-modal');
+const closeBtn = document.querySelector('.close');
+
+// Открытие модального окна при клике на кнопку
+contactBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+// Закрытие модального окна при клике на "крестик"
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Закрытие модального окна при клике вне его
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+});
